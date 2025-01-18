cb485221590f6969ec4800d9727366046aad70d6
(Raooph) <(raufquliyev829@gmail.com)>
2025-01-18 21:50:10 +0400

    Dom-1

---
+++ Dom-1/app.js
@@ -0,0 +1,23 @@
+// 1. ekranda rengli duymeler var,her duymeye click etdikde hemin reng ekrana yazilsin
+// const circles = document.getElementsByClassName("circle");
+// for (let circle of circles) {
+//   circle.onclick = () => {
+//     console.log(circle.innerText);
+//   };
+// }
+// 2. lampa
+// const circles = document.getElementsByClassName("circle");
+// const box = document.getElementById("box");
+// for (let circle of circles) {
+//   circle.onclick = () => {
+//     box.style.backgroundColor = circle.innerText;
+//   };
+// }
+// const arr = [
+//   { id: 0, name: "Phone", price: "100 AZN" },
+//   { id: 2, name: "Computer", price: "200 AZN" },
+// ];
+// for (let i of arr) {
+//   console.log(i);
+// }
+// // ekranda mehsullari gostereceksiniz
