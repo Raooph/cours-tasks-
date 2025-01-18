2bd0b7827340b1847b285b7083843ad7b6f6175b
(Raooph) <(raufquliyev829@gmail.com)>
2025-01-18 22:54:15 +0400

    dom-1 lampa

--- Dom-1/app.js
+++ Dom-1/app.js
@@ -13,6 +13,21 @@
 //     box.style.backgroundColor = circle.innerText;
 //   };
 // }
+const btn = document.querySelector("#btn");
+const lamp = document.querySelector("#lamp");
+
+const islamp = lamp.getAttribute("src");
+let isTrue = true;
+btn.onclick = () => {
+  if (isTrue) {
+    isTrue = false;
+    lamp.setAttribute("src", "boff.gif");
+  } else {
+    isTrue = true;
+    lamp.setAttribute("src", "aon.gif");
+  }
+};
+// ekranda mehsullari gostereceksiniz
 // const arr = [
 //   { id: 0, name: "Phone", price: "100 AZN" },
 //   { id: 2, name: "Computer", price: "200 AZN" },
@@ -20,4 +35,3 @@
 // for (let i of arr) {
 //   console.log(i);
 // }
-// // ekranda mehsullari gostereceksiniz
