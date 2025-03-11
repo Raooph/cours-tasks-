f501306222f86f2ed7ec7bb8da4e3317b0720f75
(Raooph) <(raufquliyev829@gmail.com)>
2025-03-11 20:58:34 +0400

    loader updated

--- practice-project/src/Loader.jsx
+++ practice-project/src/Loader.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Loader = () => {
   return (
    
-      <div class="flex items-center justify-center min-h-screen bg-gray-100">
-  <div class="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+  <div className="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
 </div>
 
   )
