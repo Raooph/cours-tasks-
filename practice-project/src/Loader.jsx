6abf079dc007d5f0a0cae9ddb87267a0fcf6617e
(Raooph) <(raufquliyev829@gmail.com)>
2025-03-11 20:50:20 +0400

    loader created

---
+++ practice-project/src/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+   
+      <div class="flex items-center justify-center min-h-screen bg-gray-100">
+  <div class="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+</div>
+
+  )
+}
+
+export default Loader
