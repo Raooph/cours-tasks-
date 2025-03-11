3f0229c1ea6fc4e0bfefe8189d6eb836a1db2eed
(Raooph) <(raufquliyev829@gmail.com)>
2025-03-12 00:40:12 +0400

    NotFound page created

---
+++ practice-project/src/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+      <div class="flex items-center justify-center min-h-screen bg-gray-100">
+  <div class="text-center">
+    <h1 class="text-6xl font-bold text-gray-800">404</h1>
+    <p class="text-xl text-gray-600 mt-2">Oops! Page not found.</p>
+    <a href="/" class="mt-4 inline-block px-6 py-2 text-white bg-blue-500 rounded hover:bg-blue-600">Go Home</a>
+  </div>
+</div>
+    
+  )
+}
+
+export default NotFound
