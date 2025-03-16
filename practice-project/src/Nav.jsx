212e02647dd034f3518edc3521c73430bd986d5c
(Raooph) <(raufquliyev829@gmail.com)>
2025-03-16 19:32:59 +0400

    basket count completed

--- practice-project/src/Nav.jsx
+++ practice-project/src/Nav.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { SlBasket } from "react-icons/sl";
 import { Link } from 'react-router'
+import UseBasket from './Store/Basket';
 const Nav = ({searchedText,setSearchedText}) => {
+  
   const links=[{id:0,to:"/",title:"Home"},{id:1,to:"/about",title:"About"},{id:2,to:"/products",title:"Products"},{id:3,to:"/contact",title:"Contact"}]
+  const {basket}=UseBasket();
+  
   return (
 
        <>
@@ -20,7 +24,7 @@ const Nav = ({searchedText,setSearchedText}) => {
         <input value={searchedText} onChange={(e)=>setSearchedText(e.target.value)} type="text" placeholder='search' className='border outline-0 px-2 py-1 rounded-2xl placeholder:text-xl placeholder:text-white text-xl p-5' />
        <Link to="/basket" className='relative' >
         <SlBasket className='text-2xl '/>
-        <span className='absolute top-[-10px] left-[-16px] text-[16px] bg-red-400 text-white w-5 h-5 flex justify-center  items-center rounded-full'>0</span>
+        <span className='absolute top-[-10px] left-[-16px] text-[16px] bg-red-400 text-white w-5 h-5 flex justify-center  items-center rounded-full'>{basket.length}</span>
         </Link>
        </div>
       </div>
